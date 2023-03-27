59dd47009c67ae64a6801f9ff3fe195fffcd11e1
damar anggoro <damaranggoro7@gmail.com>
2023-03-27 22:40:59 +0700

    create request

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express()
 const PORT = 8080;
 
+app.get("/", (req,res) => {
+    const path = require("path");
+    res.status(200).sendFile(path.join(__dirname, "/src/html/welcome.html"))
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
     console.log(`Welcome`);
