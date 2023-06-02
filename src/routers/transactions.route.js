4986baa9a464014a4a36b29ba471cf84b51b828f
digi-squad <digitalsquadfw14@proton.me>
2023-06-02 19:03:42 +0700

    finished

--- src/routers/transactions.route.js
+++ src/routers/transactions.route.js
@@ -8,28 +8,10 @@ const transactionRouter = Router();
 
 transactionRouter.post("/", checkToken, transactionsController.createTransactions);
 transactionRouter.get("/", checkToken, transactionsController.getHistory);
-// transactionRouter.get("/getallorder", checkToken, checkRole, transactionsController)
 transactionRouter.patch("/paid/:id", checkToken, checkRole, transactionsController.setPaidOrders);
-transactionRouter.get(
-    "/getpaid",
-    checkToken,
-    checkRole,
-    transactionsController.getPaidOrders
-  );
-
-  transactionRouter.get(
-    "/getpending",
-    checkToken,
-    checkRole,
-    transactionsController.getPendingOrders
-  );
-
-  transactionRouter.get(
-    "/getall",
-    checkToken,
-    checkRole,
-    transactionsController.getAllOrders
-  );
+transactionRouter.get("/getpaid", checkToken, checkRole, transactionsController.getPaidOrders);
+transactionRouter.get("/getpending", checkToken, checkRole, transactionsController.getPendingOrders);
+transactionRouter.get("/getall", checkToken, checkRole, transactionsController.getAllOrders);
 transactionRouter.patch("/cancel/:id", checkToken, checkRole, transactionsController.setCancelOrders);
 
 module.exports = transactionRouter
