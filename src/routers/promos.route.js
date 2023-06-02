1f9ed64c1ec2800d778240594cb04f13e9390656
digi-squad <digitalsquadfw14@proton.me>
2023-06-02 18:51:51 +0700

    failing row contains on add promo

--- src/routers/promos.route.js
+++ src/routers/promos.route.js
@@ -7,18 +7,9 @@ const { checkToken } = require("../middlewares/auth");
 
 promosRouter.get("/", promoController.getPromos);
 promosRouter.get("/:promoId", promoController.getPromoDetails);
+//failing row contains
 promosRouter.post("/", checkToken, checkRole, promoController.addPromo);
-promosRouter.patch(
-    "/:promoId",
-    checkToken,
-    checkRole,
-    promoController.editPromo
-  );
-  promosRouter.delete(
-    "/delete/:promoId",
-    checkToken,
-    checkRole,
-    promoController.deletePromo
-  );
+promosRouter.patch("/:promoId", checkToken, checkRole, promoController.editPromo);
+promosRouter.delete("/delete/:promoId", checkToken, checkRole, promoController.deletePromo);
 
 module.exports = promosRouter;
