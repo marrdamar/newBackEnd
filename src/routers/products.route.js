14254f72c07b42a44ecb82678749ccd692b35032
digi-squad <digitalsquadfw14@proton.me>
2023-06-09 12:59:31 +0700

    fixing add product and add promo route cloud

--- src/routers/products.route.js
+++ src/routers/products.route.js
@@ -11,7 +11,7 @@ productsRouter.get("/", productsController.getProducts);
 // /products/1
 productsRouter.get("/:productId", checkToken, productsController.getProductDetail);
 //add product with statis image
-productsRouter.post("/", checkRole, checkToken, singleUpload("image"), productsController.insertProducts);
+productsRouter.post("/", checkRole, checkToken, memoryUpload.single("image"), productsController.insertProducts);
 // productsRouter.post("/", checkRole, checkToken, memoryUpload.single("image"), productsController.insertProducts);
 //add single image
 productsRouter.patch("/image/:productId", checkToken, singleUpload("image"), productsController.patchImageProducts);
--- src/routers/promos.route.js
+++ src/routers/promos.route.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
 const promosRouter = Router();
 const promoController = require("../controllers/promos.controller");
-
+const memoryUpload = require("../middlewares/memoryUpload")
 const { checkRole } = require("../middlewares/checkRole");
 const { checkToken } = require("../middlewares/auth");
 
 promosRouter.get("/", checkToken, promoController.getPromos);
 promosRouter.get("/:promoId", checkToken, promoController.getPromoDetails);
 //failing row contains
-promosRouter.post("/", checkToken, checkRole, promoController.addPromo);
+promosRouter.post("/", checkToken, checkRole, memoryUpload.single("promo_image"), promoController.addPromo);
 promosRouter.patch("/:promoId", checkToken, checkRole, promoController.editPromo);
 promosRouter.delete("/delete/:promoId", checkToken, checkRole, promoController.deletePromo);
 
